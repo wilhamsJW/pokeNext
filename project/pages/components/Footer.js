ce40c7abaf28442e91de829a768c305a66906db9
wilhams junior <wilhams.junior@mundiale.com.br>
2023-04-16 17:04:29 -0300

    feat: added css footer

--- project/pages/components/Footer.js
+++ project/pages/components/Footer.js
@@ -1,7 +1,9 @@
+import styles from '../../styles/Footer.module.css'
+
 export default function Footer() {
     return (
-        <footer>
-            <p><span>PokeNext</span> &copy; 2021</p>
+        <footer className={styles.footer}>
+            <p><span>PokeNext</span> &copy; 2023</p>
         </footer>
     )
 }
