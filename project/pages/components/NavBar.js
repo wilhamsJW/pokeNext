03f3d1b57356f5552feec35539da3779d13f3a88
wilhams junior <wilhams.junior@mundiale.com.br>
2023-04-16 16:44:40 -0300

    feat: added css navbar

--- project/pages/components/NavBar.js
+++ project/pages/components/NavBar.js
@@ -1,9 +1,11 @@
 import Link from 'next/link'
 import Image from 'next/image'
 
+import styles from '../../styles/Navbar.module.css'
+
 export default function NavBar() {
     return (
-        <nav>
+        <nav className={styles.navbar}>
             <div>
                 <Image src="/images/pokebola.png"  width="30" height="30" alt="PokeNext" />
                 <h1>PokeNext</h1>
