5073f70914f13a0f02920a464e08f3219ef27226
wilhams junior <wilhams.junior@mundiale.com.br>
2023-04-16 14:55:11 -0300

    feat: set up the structure of the site

--- project/pages/components/Layout.js
+++ project/pages/components/Layout.js
@@ -7,7 +7,8 @@ export default function Layout({children}) {
     return (
         <> 
             <Head>
-                <link rel="shortcut icon" href="" ></link>
+                <link rel="shortcut icon" href="images/favicon.ico" ></link>
+                <title>PokeNext.JS</title>
             </Head>
             <NavBar />
             <main>{children}</main>
