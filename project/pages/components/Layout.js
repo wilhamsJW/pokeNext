03e0abbfe017c13a386d859baa52a22134baa6ae
wilhams junior <wilhams.junior@mundiale.com.br>
2023-04-16 15:07:08 -0300

    chore: added format and comments

--- project/pages/components/Layout.js
+++ project/pages/components/Layout.js
@@ -1,18 +1,18 @@
-import NavBar from "./NavBar"
-import Footer from "./Footer"
+import NavBar from "./NavBar";
+import Footer from "./Footer";
 
-import Head from 'next/head'
+import Head from "next/head";
 
-export default function Layout({children}) {
-    return (
-        <> 
-            <Head>
-                <link rel="shortcut icon" href="images/favicon.ico" ></link>
-                <title>PokeNext.JS</title>
-            </Head>
-            <NavBar />
-            <main>{children}</main>
-            <Footer />
-        </>
-    )
+export default function Layout({ children }) {
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="images/favicon.ico"></link>
+        <title>PokeNext.JS</title>
+      </Head>
+      <NavBar />
+      <main>{children}</main> {/** renderiza o que está dentro do index.tsx */}
+      <Footer />
+    </>
+  );
 }
