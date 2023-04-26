ad7f2a33f3e6ff06d4b0e34ea36d00dc4a79e079
wilhams junior <wilhams.junior@mundiale.com.br>
2023-04-25 22:55:15 -0300

    feat: added class and tags

--- project/pages/components/card.js
+++ project/pages/components/card.js
@@ -1,17 +1,24 @@
 import Image from 'next/image'
 
+import Link from 'next/link'
+
+import styles from '../../styles/Card.module.css'
+
 // o link da imagem foi adicionado em next config, para que o next entenda e permita q se trata de uma imagem externa
 
+// Esse parãmetro pokemon tá vindo doa rquivo index.tsx
 export default function Card({pokemon}) {
     return (
-        <div>
+        <div className={styles.card}>
             <Image 
                 src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
                 width="120"
                 height="120"
                 alt={pokemon.name} 
              />
-            <h3>{pokemon.name}</h3>
+             <p className={styles.id}>#{pokemon.id}</p>
+             <h3 className={styles.title}>{pokemon.name}</h3>
+             <Link className={styles.btn} href={`/pokemon/${pokemon.id}`}>Detalhes</Link>
         </div>
     )
 }
