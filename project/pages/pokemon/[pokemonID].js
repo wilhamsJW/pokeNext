6d162e2b6489cdb412192612b6455396b9076a58
wilhams junior <wilhams.junior@mundiale.com.br>
2023-04-27 23:09:31 -0300

    feat: added api's data

--- project/pages/pokemon/[pokemonID].js
+++ project/pages/pokemon/[pokemonID].js
@@ -1,41 +1,81 @@
+import Image from "next/image";
+
+import styles from "../../styles/Pokemon.module.css";
+
 // Faz mapeamento geral dos dados
 // estou passando todos os meus paths para função para q possa pré renderizar
-export const getStaticPaths = async() => {
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=251`);
+  const data = await res.json();
 
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=251`)
-    const data = await res.json()
-
-    // params
-    const paths = data.results.map((pokemon, i) => {
-        return {
-            params: {pokemonID: (i+1).toString()}
-            /** Definindo o ID que irei passar como path, coloquei i+1 pq o array
-             * começa em 0, então o pokemon zero do array será o pokemon 1 para mim
-             * dessa forma percorro todos os id da minha rota e mando eles no return abaixo
-             */
-        }
-    })
-
+  // params
+  const paths = data.results.map((pokemon, i) => {
     return {
-        paths,
-        fallback: false /** é padrão do next.js porém pode ser usado como true, veja documentação */
-    }
-}
+      params: { pokemonID: (i + 1).toString() },
+      /** Definindo o ID que irei passar como path, coloquei i+1 pq o array
+       * começa em 0, então o pokemon zero do array será o pokemon 1 para mim
+       * dessa forma percorro todos os id da minha rota e mando eles no return abaixo
+       */
+    };
+  });
+
+  return {
+    paths,
+    fallback: false /** é padrão do next.js porém pode ser usado como true, veja documentação */,
+  };
+};
 
 // faz mapeamento um a um dos dados
-export const getStaticProps = async(context) => {
-    // esse context é basicamente o que criamos acima, por isso acessamos: context.params.pokemonID
-    //que é o mesmo nome q coloamos acima
-    const id = context.params.pokemonID
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+export const getStaticProps = async (context) => {
+  // esse context é basicamente o que criamos acima, por isso acessamos: context.params.pokemonID
+  //que é o mesmo nome q coloamos acima
+  const id = context.params.pokemonID;
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
 
-    const data = await res.json()
+  const data = await res.json();
 
-    return {
-        props: { data }
-    }
-}
+  return {
+    props: { data },
+  };
+};
 
-export default function Pokemon({data}) { // Next automaticamente entende que esse parâmetro é da função getStaticProps()
-    return <p>Nome do pokemon: {data.name}</p>
+export default function Pokemon({ data }) {
+  // Next automaticamente entende que esse parâmetro é da função getStaticProps()
+  console.log("data:", data);
+
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <Image
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`}
+        width="200"
+        height="200"
+        alt={data.name}
+      />
+
+      <div>
+        <h3>Número:</h3>
+        <p>#&nbsp;&nbsp;{data.id}</p>
+      </div>
+      <div>
+        <h3>Tipo:</h3>
+        <div>
+          #&nbsp;
+          {data.types.map((e, i) => (
+            <span key={i}>&nbsp;{e.type.name}</span>
+          ))}
+        </div>
+
+        <div>
+          <h4>Atura:</h4>
+          <p>{data.height * 10} cm</p>
+        </div>
+
+        <div>
+          <h4>Peso:</h4>
+          <p>{data.height / 10} kg</p>
+        </div>
+      </div>
+    </div> // end
+  );
 }
